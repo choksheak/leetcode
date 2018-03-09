5c8b68ddfb47f945364fffb86646cb0e7129f83d
ChokSheak Lau <choksheak@gmail.com>
2018-03-08 22:52:34 -0800

    Create Minimum Window Substring (Buggy).java

---
+++ code/Minimum Window Substring (Buggy).java
@@ -0,0 +1,72 @@
+public class Solution {
+    public String minWindow(String s, String t) {
+        // Form map of chars in t to count.
+        Map<Character, Integer> tChars = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            Integer count = tChars.get(c);
+            tChars.put(c, count != null ? count + 1 : 1);
+        }
+        
+        Map<Character, Integer> seenCount = new HashMap<>();
+        TreeSet<Integer> seenIndexes = new TreeSet<>();
+        
+        int minWinStart = 0;
+        int minWinEnd = -1;
+        int minWinSize = s.length() + 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (!tChars.containsKey(c)) {
+                continue;
+            }
+            
+            Integer count0 = seenCount.get(c);
+            int count = (count0 != null) ? count0 + 1 : 1;
+            
+            // If we have too many of the same char c, remove the first c within the current window.
+            if (count > tChars.get(c)) {
+                for (int index = seenIndexes.first(); index < i; index++) {
+                    if (s.charAt(index) == c) {
+                        seenIndexes.remove(index);
+                        break;
+                    }
+                }
+            }
+            else {
+                seenCount.put(c, count);
+            }
+
+            seenIndexes.add(i);
+
+            if (seenIndexes.size() == t.length()) {
+                // Track minimum window.
+                int min = seenIndexes.pollFirst(); // remove
+                int size = i - min + 1;
+
+                if (size < minWinSize) {
+                    minWinStart = min;
+                    minWinEnd = i;
+                    minWinSize = size;
+                }
+
+                // Remove char at start index from seenCount.
+                c = s.charAt(min);
+                seenCount.put(c, seenCount.get(c) - 1);
+            }
+        }
+        
+        return minWinEnd == -1 ? "" : s.substring(minWinStart, minWinEnd + 1);
+    }
+
+    public static void main() {
+        Solution s = new Solution();
+        String answer = s.minWindow("aaaaaaaaaaaabbbbbcdd", "abcdd");
+        System.out.println(answer);
+
+        if (!answer.equals("abbbbbcdd")) {
+            System.out.println("Wrong answer");
+        }
+    }
+}
