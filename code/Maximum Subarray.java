83ba7e281be50cb2073c86397a36036194bbfd7e
ChokSheak Lau <choksheak@gmail.com>
2018-04-09 20:02:23 -0700

    Create Maximum Subarray.java

---
+++ code/Maximum Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int cur = max;
+        
+        for (int i = 1; i < nums.length; i++) {
+            cur = Math.max(nums[i], nums[i] + cur);
+            max = Math.max(max, cur);
+        }
+        
+        return max;
+    }
+}
