0a0a4ed6b81ae7315c28ddc4eb407eef9bd98943
ChokSheak Lau <choksheak@gmail.com>
2020-01-14 20:59:35 -0800

    Update Find Free Intervals.java

--- code/Find Free Intervals.java
+++ code/Find Free Intervals.java
@@ -20,57 +20,53 @@ public class Solution {
     
     private static List<Interval> findFreeIntervals(List<List<Interval>> intervals) {
         PriorityQueue<Interval> sortedIntervals = new PriorityQueue<>((i, j) -> i.start - j.start);
-        
+
         // Insert all the intervals into a sorted collection.
         for (List<Interval> list: intervals) {
-            for (Interval i: list) {
-                sortedIntervals.add(i);
-            }
+            sortedIntervals.addAll(list);
         }
-        
+
         if (sortedIntervals.size() == 0) {
             return new ArrayList<Interval>();
         }
-        
+
         // Merge them.
         List<Interval> merged = new ArrayList<>();
-        Interval in = sortedIntervals.remove();
-        int lastStart = in.start;
-        int lastEnd = in.end;
-        
-        
+        Interval interval = sortedIntervals.remove();
+        int lastStart = interval.start;
+        int lastEnd = interval.end;
+
         while (!sortedIntervals.isEmpty()) {
-            in = sortedIntervals.remove();
+            interval = sortedIntervals.remove();
             
-            if (in.start <= lastEnd) {
+            if (interval.start <= lastEnd) {
                 // Merge.
-                lastEnd = Math.max(lastEnd, in.end);
-            }
-            else {
+                lastEnd = Math.max(lastEnd, interval.end);
+            } else {
                 // Start new interval.
                 merged.add(new Interval(lastStart, lastEnd));
-                lastStart = in.start;
-                lastEnd = in.end;
+                lastStart = interval.start;
+                lastEnd = interval.end;
             }
         }
-        
+
         // Always add the current interval to the end.
         merged.add(new Interval(lastStart, lastEnd));
-        
+
         // Flip the interval.
         List<Interval> flipped = new ArrayList<>();     
         lastEnd = merged.get(0).end;
-        
+
         if (merged.get(0).start > 0) {
             flipped.add(new Interval(0, merged.get(0).start));
         }
-        
+
         for (int i = 1; i < merged.size(); i++) {
-            in = merged.get(i);
-            flipped.add(new Interval(lastEnd, in.start));
-            lastEnd = in.end;
+            interval = merged.get(i);
+            flipped.add(new Interval(lastEnd, interval.start));
+            lastEnd = interval.end;
         }
-        
+
         return flipped;
     }
     
