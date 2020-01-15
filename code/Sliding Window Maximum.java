3723b0610f6adbf290f0ccec9c0236a8a212fb73
ChokSheak Lau <choksheak@gmail.com>
2020-01-14 21:41:12 -0800

    Update Sliding Window Maximum.java

--- code/Sliding Window Maximum.java
+++ code/Sliding Window Maximum.java
@@ -1,8 +1,52 @@
-// Given an array nums, there is a sliding window of size k which is moving from
-// the very left of the array to the very right. You can only see the k numbers
-// in the window. Each time the sliding window moves right by one position.
-// Return the max sliding window.
+/*
+Given an array nums, there is a sliding window of size k which is moving from
+the very left of the array to the very right. You can only see the k numbers
+in the window. Each time the sliding window moves right by one position.
+Return the max sliding window.
+*/
 
+// This is the common optimal solution which is O(N).
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 || k == 0) {
+            return new int[0];
+        }
+        
+        int[] answer = new int[n - k + 1];
+        int answerIndex = 0;
+        LinkedList<Integer> window = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+            // Remove numbers that fell out from the left side.
+            while (!window.isEmpty() && window.getFirst() <= i - k) {
+                window.removeFirst();
+            }
+            
+            // Remove any numbers from the right side that are smaller or equal to
+            // the current number, since they will not affect the window maximum
+            // after we add the current number.
+            while (!window.isEmpty() && nums[window.getLast()] <= nums[i]) {
+                window.removeLast();
+            }
+            
+            // Add current index to right side of window.
+            window.addLast(i);
+            
+            // Skip first k-1 numbers.
+            if (i >= k - 1) {
+                // The leftmost number in the window must be the current largest
+                // number, because if this were not true, then we would already
+                // have removed it earlier.
+                answer[answerIndex++] = nums[window.getFirst()];
+            }
+        }
+        
+        return answer;
+    }
+}
+
+// Non-optimal O(N log K) solution using heap.
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (k == 0) return new int[0];
